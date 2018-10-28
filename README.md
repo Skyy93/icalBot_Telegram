4e727410bd2c235140f1a88725cf43f0a26a1bf8
Skyy <f.deuser93@hotmail.de>
2018-10-28 20:53:36 +0100

    First commit with code and test-files

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# icalBot_Telegram
+# iCal-Reminderbot for Telegram
+
+This bot will remind you in telegram with messages and reads in a ics-file
+
+
+### Prerequisites
+
+```sh
+$ pip install icalender
+$ pip install python-telegram-bot --upgrade
+$ pip install sqlite3
+```
+And use your Bot-Token for:
+
+```py
+updater = Updater("TOKEN")
+```
---
+++ reminderBotBetty.py
@@ -0,0 +1,146 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Simple Bot to read in .ics files and sending reminders
+
+
+"""
+This Bot uses the Updater class from the python-telegram-bot API and the icalender Parser
+
+Please use your own Telegram-Token in the main-Class. After you started the bot it will run till you press Ctrl-C on the command line
+
+Usage:
+The Bot is started on the command line and reads in the icalender-file.
+Send /start to initiate the reminder-service. Once a day the bot checks if there are any reminders for today and sends the messages to the subscribers.
+If you don't want to get reminders send /stop for getting deleted from the database.
+"""
+
+from telegram.ext import Updater, CommandHandler
+from icalendar import Calendar, Event
+from datetime import datetime
+import requests
+import logging
+import time
+import sqlite3
+import os
+
+# Enable logging
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
+
+logger = logging.getLogger(__name__)
+
+# Create DB and connect if it does not exists
+if not os.path.isfile('data.db'):
+    conn = sqlite3.connect('data.db')
+    c = conn.cursor()
+    c.execute('''CREATE TABLE users
+            (users text)''')
+    conn.commit()
+    conn.close()
+
+# Imports ics-file
+def importCalender():
+    g = open('termine.ics','rb')
+    gcal = Calendar.from_ical(g.read())
+    g.close()
+    return gcal
+
+# Sends Reminders for today when first /start sent
+def sendReminders(bot, job):
+    events = todayReminder()
+    for message in events:
+        bot.send_message(job.context, message)
+
+# Builds text for today 
+def todayReminder():
+    gcal = importCalender()
+    today = datetime.today().date()
+    botText = []
+    for component in gcal.walk():
+        if component.get('dtstart') is not None:
+            if component.get('dtstart').dt.date() == today:
+                botText.append("Termin: " +str(component.get('summary')) + "\nDatum: " + str(component.get('DTSTART').dt.strftime("%d-%m-%Y %H:%M")) + "\nLocation: " + str(component.get('LOCATION')) + "\nBeschreibung: " + str(component.get('DESCRIPTION')))
+    return botText
+
+
+# Define a few command handlers. These usually take the two arguments bot and
+def start(bot, update, args, job_queue, chat_data):
+    conn = sqlite3.connect('data.db')
+    c = conn.cursor()
+    update.message.reply_text('Hi im Betty-Bot and will inform you about your events :)')
+    chat_id = update.message.chat_id
+    job = job_queue.run_once(sendReminders, 1, context=chat_id)
+    chat_data['job'] = job
+
+    rowsWithId = c.execute("SELECT * FROM users WHERE users='"+str(chat_id)+"'")
+    if rowsWithId.fetchone() is None: 
+        c.execute("INSERT INTO users VALUES ('"+str(chat_id)+"')")
+    conn.commit()
+    conn.close()
+
+# Removes the users from the database
+def stop(bot, update, args, job_queue, chat_data):
+    update.message.reply_text('Okay, i delete you from the database and you will not get any further reminders.')
+    conn = sqlite3.connect('data.db')
+    c = conn.cursor()
+    chat_id = update.message.chat_id
+    c.execute("DELETE FROM users WHERE users='"+str(chat_id)+"'")
+    conn.commit()
+    conn.close()
+
+# DailyJob for the reminders
+def dailyJob(bot):
+    while(1):
+        time.sleep(86400)
+        conn = sqlite3.connect('data.db')
+        c = conn.cursor()
+        c.execute("SELECT * FROM users")
+        users = c.fetchall()
+        events = todayReminder()
+        for id in users:
+            for message in events:
+                bot.sendMessage(id[0],message)
+            bot.sendMessage(id[0],"If you dont want to get this reminders send: /stop.")
+        conn.commit()
+        conn.close()   
+        
+
+
+# Error handling
+def error(bot, update, error):
+    """Log Errors caused by Updates."""
+    logger.warning('Update "%s" caused error "%s"', update, error)
+
+
+def main():
+    """Run bot."""
+    updater = Updater("TOKEN")
+
+    # Get the dispatcher to register handlers
+    dp = updater.dispatcher
+
+    # on different commands - answer in Telegram
+    dp.add_handler(CommandHandler("start", start,
+                                  pass_args=True,
+                                  pass_job_queue=True,
+                                  pass_chat_data=True))
+    dp.add_handler(CommandHandler("stop", stop,
+                                  pass_args=True,
+                                  pass_job_queue=True,
+                                  pass_chat_data=True))
+    
+
+    # log all errors
+    dp.add_error_handler(error)
+
+    # Start the Bot
+    updater.start_polling()
+
+    #DailyUpdate
+    dailyJob(updater.bot)
+
+
+
+if __name__ == '__main__':
+    main()
